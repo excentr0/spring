e8be3d3fcda7850da74db490f80e7a536978dc20
Mikhail Mishin <excentro@gmail.com>
2020-09-06 11:41:32 +0300

    lesson 3 end

--- lesson-3-hibernate/src/main/java/com/excentro/Main.java
+++ lesson-3-hibernate/src/main/java/com/excentro/Main.java
@@ -4,13 +4,13 @@ import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
     EntityManagerFactory emFactory =
         new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 
+    // Insert into database
     /*EntityManager entityManager = emFactory.createEntityManager();
     User user1 = new User(null, "Alex", "alex");
     User user2 = new User(null, "Mike", "mike");
@@ -21,19 +21,36 @@ public class Main {
     entityManager.getTransaction().commit();
     entityManager.close();*/
 
-    EntityManager entityManager = emFactory.createEntityManager();
+    // select from database
+    //    EntityManager entityManager = emFactory.createEntityManager();
+    //
+    //    User user = entityManager.find(User.class, 1);
+    //    System.out.println(user);
+    //
+    //    List<User> users = entityManager.createQuery("from User", User.class).getResultList();
+    //    System.out.println(users);
+    //
+    //    User singleResult =
+    //        entityManager
+    //            .createQuery("from User where login=:login", User.class)
+    //            .setParameter("login", "mike")
+    //            .getSingleResult();
+    //    System.out.println(singleResult);
 
+    // update entity
+    /*EntityManager entityManager = emFactory.createEntityManager();
     User user = entityManager.find(User.class, 1);
-    System.out.println(user);
 
-    List<User> users = entityManager.createQuery("from User", User.class).getResultList();
-    System.out.println(users);
+    entityManager.getTransaction().begin();
+    user.setPassword("pass");
+    entityManager.getTransaction().commit();*/
 
-    User singleResult =
-        entityManager
-            .createQuery("from User where login=:login", User.class)
-            .setParameter("login", "mike")
-            .getSingleResult();
-    System.out.println(singleResult);
+    // add Contact to User
+    EntityManager entityManager = emFactory.createEntityManager();
+    User user = entityManager.find(User.class, 1);
+    Contact contact = new Contact(null, "mobile phone", "123232323", user);
+    entityManager.getTransaction().begin();
+    entityManager.persist(contact);
+    entityManager.getTransaction().commit();
   }
 }
