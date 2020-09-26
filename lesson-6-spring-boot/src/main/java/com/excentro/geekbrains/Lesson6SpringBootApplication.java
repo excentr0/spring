166c2a855f2b7d27e83f7650a88d1e30803393c4
Mikhail Mishin <excentro@gmail.com>
2020-09-26 14:41:41 +0300

    готово 6-е задание

---
+++ lesson-6-spring-boot/src/main/java/com/excentro/geekbrains/Lesson6SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.excentro.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson6SpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Lesson6SpringBootApplication.class, args);
+  }
+}
---
+++ lesson-6-spring-boot/src/main/java/com/excentro/geekbrains/persistence/repo/ProductSpecification.java
@@ -0,0 +1,33 @@
+package com.excentro.geekbrains.persistence.repo;
+
+import com.excentro.geekbrains.persistence.entity.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+
+public final class ProductSpecification {
+  private ProductSpecification() {}
+
+  public static Specification<Product> trueLiteral() {
+    return (root, query, builder) -> builder.isTrue(builder.literal(true));
+  }
+
+  public static Specification<Product> titleLike(String title) {
+    return (root, query, builder) -> builder.like(root.get("title"), "%" + title + "%");
+  }
+
+  public static Specification<Product> priceLess(BigDecimal price) {
+    return (root, query, builder) -> builder.lessThan(root.get("cost"), price);
+  }
+
+  public static Specification<Product> priceGreater(BigDecimal price) {
+    return (root, query, builder) -> builder.greaterThan(root.get("cost"), price);
+  }
+
+  public static Specification<Product> priceBetween(BigDecimal priceLo, BigDecimal priceHi) {
+    return (root, query, builder) ->
+        builder.and(
+            builder.greaterThan(root.get("cost"), priceLo),
+            builder.lessThan(root.get("cost"), priceHi));
+  }
+}
